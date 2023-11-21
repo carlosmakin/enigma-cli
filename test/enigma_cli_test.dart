9e34efe084b9f86baf4c210ed9d0c4525c343f23
Carlos Makin <carlos@makin.dev>
2023-11-21 15:32:08 -0700

    Update enigma_cli_test.dart

--- test/enigma_cli_test.dart
+++ test/enigma_cli_test.dart
@@ -5,7 +5,7 @@ import 'dart:typed_data';
 import 'package:args/command_runner.dart';
 import 'package:enigma_cli/enigma_cli.dart';
 import 'package:test/test.dart';
-import 'package:enigma/enigma.dart' as enigma;
+import 'package:enigma/enigma.dart';
 
 void main() {
   final CommandRunner<String> runner = CommandRunner<String>(name, description);
@@ -15,54 +15,30 @@ void main() {
     runner.addCommand(command);
 
     test('generates random key', () async {
-      final String? aes128Key = await runner.run(
-        <String>['keygen', '-r', '-e', '128'],
-      );
-      expect(base64.decode(aes128Key!).length, 16);
-
-      final String? aes192Key = await runner.run(
-        <String>['keygen', '-r', '-e', '192'],
-      );
-      expect(base64.decode(aes192Key!).length, 24);
-
-      final String? aes256Key = await runner.run(
-        <String>['keygen', '-r', '-e', '256'],
-      );
-      expect(base64.decode(aes256Key!).length, 32);
+      for (final AESKeyStrength strength in AESKeyStrength.values) {
+        final String? key = await runner.run(
+          <String>['keygen', '-r', '-e', '${strength.numBits}'],
+        );
+        expect(base64.decode(key!).length, strength.numBytes);
+      }
     });
 
     test('derives key from passphrase without salt', () async {
-      final String? aes128Key = await runner.run(
-        <String>['keygen', '-p', 'test', '-e', '128', '-i', '1'],
-      );
-      expect(base64.decode(aes128Key!).length, 16);
-
-      final String? aes192Key = await runner.run(
-        <String>['keygen', '-p', 'test', '-e', '192', '-i', '1'],
-      );
-      expect(base64.decode(aes192Key!).length, 24);
-
-      final String? aes256Key = await runner.run(
-        <String>['keygen', '-p', 'test', '-e', '256', '-i', '1'],
-      );
-      expect(base64.decode(aes256Key!).length, 32);
+      for (final AESKeyStrength strength in AESKeyStrength.values) {
+        final String? key = await runner.run(
+          <String>['keygen', '-p', 'test', '-e', '${strength.numBits}', '-i', '1'],
+        );
+        expect(base64.decode(key!).length, strength.numBytes);
+      }
     });
 
     test('derives key from passphrase with salt', () async {
-      final String? aes128Key = await runner.run(
-        <String>['keygen', '-p', 'test', '-s', 'salt', '-e', '128', '-i', '1'],
-      );
-      expect(base64.decode(aes128Key!).length, 16);
-
-      final String? aes192Key = await runner.run(
-        <String>['keygen', '-p', 'test', '-s', 'salt', '-e', '192', '-i', '1'],
-      );
-      expect(base64.decode(aes192Key!).length, 24);
-
-      final String? aes256Key = await runner.run(
-        <String>['keygen', '-p', 'test', '-s', 'salt', '-e', '256', '-i', '1'],
-      );
-      expect(base64.decode(aes256Key!).length, 32);
+      for (final AESKeyStrength strength in AESKeyStrength.values) {
+        final String? key = await runner.run(
+          <String>['keygen', '-p', 'test', '-s', 'salt', '-e', '${strength.numBits}', '-i', '1'],
+        );
+        expect(base64.decode(key!).length, strength.numBytes);
+      }
     });
   });
 
@@ -71,11 +47,13 @@ void main() {
     runner.addCommand(command);
 
     test('encrypts plaintext with a given key', () async {
-      final String key = base64.encode(List<int>.filled(32, 0));
-      final String? cipherText = await runner.run(
-        <String>['encrypt-text', '-i', 'plaintext', '-k', key],
-      );
-      expect(cipherText, isNotNull);
+      for (final AESKeyStrength strength in AESKeyStrength.values) {
+        final String key = base64.encode(List<int>.filled(strength.numBytes, 64));
+        final String? cipherText = await runner.run(
+          <String>['encrypt-text', '-i', 'plaintext', '-k', key],
+        );
+        expect(cipherText, isNotNull);
+      }
     });
   });
 
@@ -84,16 +62,17 @@ void main() {
     runner.addCommand(command);
 
     test('decrypts ciphertext with a given key', () async {
-      final Uint8List key = Uint8List.fromList(List<int>.filled(32, 0));
-      final Uint8List iv = Uint8List.fromList(List<int>.filled(16, 0));
-      final String base64Key = base64.encode(key);
+      for (final AESKeyStrength strength in AESKeyStrength.values) {
+        final Uint8List key = Uint8List.fromList(List<int>.filled(strength.numBytes, 64));
+        final Uint8List iv = Uint8List.fromList(List<int>.filled(16, 64));
+        final String base64Key = base64.encode(key);
 
-      final String encryptedData =
-          enigma.encryptTextWithEmbeddedIV(key: key, iv: iv, text: 'plaintext');
-      final String? decryptedText = await runner.run(
-        <String>['decrypt-text', '-i', encryptedData, '-k', base64Key],
-      );
-      expect(decryptedText, 'plaintext');
+        final String encryptedData = encryptTextWithEmbeddedIV(key: key, iv: iv, text: 'plaintext');
+        final String? decryptedText = await runner.run(
+          <String>['decrypt-text', '-i', encryptedData, '-k', base64Key],
+        );
+        expect(decryptedText, 'plaintext');
+      }
     });
   });
 
@@ -102,17 +81,19 @@ void main() {
     runner.addCommand(encryptCommand);
 
     test('encrypts a file with a given key', () async {
-      final String key = base64.encode(List<int>.filled(32, 0));
-      final File inputFile = await File('input.txt').writeAsString('Input file content');
+      for (final AESKeyStrength strength in AESKeyStrength.values) {
+        final String key = base64.encode(List<int>.filled(strength.numBytes, 64));
+        final File inputFile = await File('input.txt').writeAsString('Input file content');
 
-      final String? response = await runner.run(
-        <String>['encrypt-file', '-i', inputFile.path, '-k', key],
-      );
-      expect(response, contains('File encrypted'));
-      expect(File('${inputFile.path}.aes256').existsSync(), isTrue);
+        final String? response = await runner.run(
+          <String>['encrypt-file', '-i', inputFile.path, '-k', key],
+        );
+        expect(response, contains('File encrypted'));
+        expect(File('${inputFile.path}.aes${strength.numBits}').existsSync(), isTrue);
 
-      await inputFile.delete();
-      await File('${inputFile.path}.aes256').delete();
+        await inputFile.delete();
+        await File('${inputFile.path}.aes${strength.numBits}').delete();
+      }
     });
   });
 
@@ -121,30 +102,32 @@ void main() {
     runner.addCommand(decryptCommand);
 
     test('decrypts a file with a given key', () async {
-      final String key = base64.encode(List<int>.filled(32, 0));
-      final File inputFile = await File('input.txt').writeAsString('Input file content');
+      for (final AESKeyStrength strength in AESKeyStrength.values) {
+        final String key = base64.encode(List<int>.filled(strength.numBytes, 64));
+        final File inputFile = await File('input.txt').writeAsString('Input file content');
 
-      String? response = await runner.run(
-        <String>['encrypt-file', '-i', inputFile.path, '-k', key],
-      );
-      expect(response, contains('File encrypted'));
+        String? response = await runner.run(
+          <String>['encrypt-file', '-i', inputFile.path, '-k', key],
+        );
+        expect(response, contains('File encrypted'));
 
-      await inputFile.delete();
-      expect(File('input.txt').existsSync(), isFalse);
+        await inputFile.delete();
+        expect(File('input.txt').existsSync(), isFalse);
 
-      expect(File('${inputFile.path}.aes256').existsSync(), isTrue);
-      final File outputFile = File('input.txt.aes256');
+        expect(File('${inputFile.path}.aes${strength.numBits}').existsSync(), isTrue);
+        final File outputFile = File('input.txt.aes${strength.numBits}');
 
-      response = await runner.run(
-        <String>['decrypt-file', '-i', outputFile.path, '-k', key],
-      );
+        response = await runner.run(
+          <String>['decrypt-file', '-i', outputFile.path, '-k', key],
+        );
 
-      expect(response, contains('File decrypted'));
-      expect(File('input.txt').existsSync(), isTrue);
-      expect(await File('input.txt').readAsString(), 'Input file content');
+        expect(response, contains('File decrypted'));
+        expect(File('input.txt').existsSync(), isTrue);
+        expect(await File('input.txt').readAsString(), 'Input file content');
 
-      await outputFile.delete();
-      await File('input.txt').delete();
+        await outputFile.delete();
+        await File('input.txt').delete();
+      }
     });
   });
 }
